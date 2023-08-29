f109aaf8d1b888e492d8c2cb239e1e1b54e0b142
EElBard <ethan.elbard@gmail.com>
2023-08-29 11:36:25 -0700

    Refactored to use Axios and Cheerio again

--- index.js
+++ index.js
@@ -3,42 +3,56 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const app = express()
-const cookiejarsupport = require('axios-cookiejar-support')
-const toughcookie = require('tough-cookie')
-
-const wrapper = cookiejarsupport.wrapper()
-const jar = new toughcookie.CookieJar();
-const client = wrapper(axios.create({jar}))
-
 
 const tracking = [
     {
         name: 'beetlejuice',
         address: 'https://thesmithcenter.com/tickets/2324/beetlejuice/'
     }
-]
+];
 
-const tickets = []
+const tickets = [];
 
-tracking.forEach(show => {
-    client.get(show.address)
-        .then((response) => {
-            const html = response.data
-            console.log(response.data)
-            //const $ = cheerio.load(html)
+// Assuming you have already made the necessary imports and set up the tracking array
 
-            let availNone = $document.querySelectorAll('[data-availability=0]');
-            let availOne = document.querySelectorAll('[data-availability=1]');
-            let availTwo = document.querySelectorAll('[data-availability=2]');
-            let availThree = document.querySelectorAll('[data-availability=3]');
-            
-            for(i = 0; i < availNone.length, i++;){
-                tickets.push({
-                    item: availNone[i]
-                })
-            }
-        })
-})
+async function checkAttributeChange() {
+    try {
+        const promises = tracking.map(async (show) => {
+            // Make an HTTP GET request to the website
+            const response = await axios.get(show.address);
+
+            // Load the HTML content into cheerio
+            const $ = cheerio.load(response.data);
+
+            // Select all <li> elements with the data-availability attribute
+            const liElements = $('[data-availability]');
+
+            // Iterate through the <li> elements and check the data-availability attribute
+            liElements.each((index, liElement) => {
+                const currentAvailability = $(liElement).attr('data-availability');
+
+                // Check if the availability has changed
+                if (currentAvailability !== '0') {
+                    const changeDetails = {
+                        showName: show.name,
+                        elementIndex: index + 1,
+                        newAvailability: currentAvailability
+                    };
+                    tickets.push(changeDetails);
+                }
+            });
+        });
+
+        // Wait for all promises to resolve
+        await Promise.all(promises);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Call the function to start monitoring
+checkAttributeChange();
+
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Ticket Availability API')
